486036975ac51c13a38913aa00d8c35adfc493d5
tester4 <tester4@giftrete.com>
2021-11-19 01:21:38 +0000

    Add files

---
+++ Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace QA.Contribution.Test.Journey
+{
+    public static class Extensions
+    {
+        public static ConfigurationBuilder GetCurrentDirectory(this ConfigurationBuilder configurationBuilder, out string currentDirectory)
+        {
+            currentDirectory = GetCurrentDirectory();
+            return configurationBuilder;
+        }
+
+        public static string GetCurrentDirectory()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+        }
+
+        public static void AppendReport(this Exception exception, string message)
+        {
+            Console.WriteLine($"{message} /n Exception thrown: {exception}");
+        }
+
+        public static void AppendReport(this string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static IWebElement GetClickableElement(this IWebDriver driver, By by)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(120))
+            .Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+
+        public static SelectElement GetSelectElement(this IWebDriver driver, By by)
+        {
+            return new SelectElement(driver.FindElement(by));
+        }
+    }
+}
---
+++ Hook/ContributionScenarioHook.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using OpenQA.Selenium;
+
+using TechTalk.SpecFlow;
+
+namespace QA.Contribution.Test.Journey.Hook
+{
+    [Binding]
+    public class ContributionScenarioHook
+    {
+        private ScenarioContext _context;
+
+        public ContributionScenarioHook(ScenarioContext context)
+        {
+            _context = context;
+        }
+
+        [BeforeScenario(Order = 0)]
+        public void BeforeScenarioCreateWebDr()
+        {
+            try
+            {
+                _context.Set(WebDriverBuilder.CreateNew(), ScenarioContextConstants.WebDriver);
+            }
+            catch (Exception e)
+            {
+                e.AppendReport("Failed to create web driver.");
+                throw;
+            }
+        }
+
+        [AfterScenario(Order = 0)]
+        public void QuitDriver()
+        {
+            try
+            {
+                _context.Get<IWebDriver>(ScenarioContextConstants.WebDriver).Quit();
+            }
+            catch (Exception e)
+            {
+                e.AppendReport("Failed to quit web driver.");
+            }
+        }
+
+        [AfterScenario(Order = 1)]
+        public void RemoveDriverFromContext()
+        {
+            try
+            {
+                _context.Remove(ScenarioContextConstants.WebDriver);
+            }
+            catch (Exception e)
+            {
+                e.AppendReport("Removing driver from context has failed.");
+            }
+        }
+    }
+}
